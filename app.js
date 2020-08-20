be6d603838257365bd1da2c6d4ba657691f9fe57
Emeka Okoli <emekaokoli00@gmail.com>
2020-08-20 18:28:59 +0100

    Passport

--- app.js
+++ app.js
@@ -12,9 +12,14 @@ const leaderRouter = require('./routes/leaderRouter')
 const Dishes = require('./models/dishes')
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
-
-var app = express()
+var passport = require('passport')
+var authenticate = require('./authenticate')
 const mongoose = require('mongoose')
+var app = express()
+
+
+
+
 
 const url = 'mongodb://localhost:27017/conFusion'
 const connect = mongoose.connect(url)
@@ -45,24 +50,21 @@ app.use(
     store: new FileStore(),
   }),
 )
+
+app.use(passport.initialize())
+app.use(passport.session())
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
 
 function auth(req, res, next) {
-  console.log(req.session)
+  console.log(req.user)
 
-  if (!req.session.user) {
+  if (!req.user) {
     var err = new Error('You are not authenticated!')
     err.status = 403
-    return next(err)
+    next(err)
   } else {
-    if (req.session.user === 'authenticated') {
-      next()
-    } else {
-      var err = new Error('You are not authenticated!')
-      err.status = 403
-      return next(err)
-    }
+    next()
   }
 }
 
